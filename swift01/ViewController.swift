46a437147436bd4e67763ab73bf889928ff19f29
csnam-apple <centauri515@gmail.com>
2022-11-22 10:43:34 +0900

    Initial Commit

---
+++ swift01/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift01
+//
+//  Created by 비바 on 2022/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
